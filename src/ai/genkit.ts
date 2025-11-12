0378f11d311ec900580e4e663df0d557ca5f4229
MD Parves <parvesmd270@gmail.com>
2025-11-12 19:10:26 +0000

    GenkitError: NOT_FOUND: Model 'googleai/gemini-2.0-flash' not found

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,3 +1,4 @@
+
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
 import { config } from 'dotenv';
@@ -18,5 +19,5 @@ if (apiKey) {
 
 export const ai = genkit({
   plugins,
-  model: 'googleai/gemini-2.0-flash',
+  model: 'googleai/gemini-1.5-flash-latest',
 });
