fa670e43fdbe43ded55605832666d304d3e8df76
MD Parves <parvesmd270@gmail.com>
2025-11-14 15:14:01 +0000

    Thank you for your business! support@bazaargo.com  update Thank you for

--- src/components/admin/order-invoice.tsx
+++ src/components/admin/order-invoice.tsx
@@ -111,7 +111,7 @@ export const OrderInvoice = React.forwardRef<HTMLDivElement, OrderInvoiceProps>(
             
             <footer className="mt-16 pt-4 border-t-2 text-center text-gray-500 text-sm">
                 <p>Thank you for your business!</p>
-                <p>{settings.contactEmail}</p>
+                <p>support@Menswell.com</p>
             </footer>
         </div>
     );
