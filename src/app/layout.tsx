138ac44d561d778519d2ab772fb01f3df2f79420
MD Parves <parvesmd270@gmail.com>
2025-11-21 08:58:44 +0000

    আমি google টেক ম্যানেজার এড করতে চাচ্ছিলাম আর কি  এটা তুমি এখন কি করে দা

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,6 +27,20 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Hind+Siliguri:wght@400;500;600;700&family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet"></link>
         
+        {/* Google Tag Manager */}
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KTVBFBK2');`
+          }}
+        />
+        {/* End Google Tag Manager */}
+
         {/* Meta Pixel Code */}
         <Script
           id="fb-pixel"
@@ -57,7 +71,12 @@ export default function RootLayout({
         {/* End Meta Pixel Code */}
 
       </head>
-      <body className="font-body antialiased min-h-screen" suppressHydrationWarning>
+      <body className="font-body antialiased" suppressHydrationWarning>
+        {/* Google Tag Manager (noscript) */}
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KTVBFBK2"
+        height="0" width="0" style={{display:'none',visibility:'hidden'}}></iframe></noscript>
+        {/* End Google Tag Manager (noscript) */}
+
         <ThemeProvider>
             <WishlistProvider>
               <CartProvider>
